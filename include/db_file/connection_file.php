2dc576868e375d039ac4f65d0a3af143df3c7c03
himanshu <himanshusaini26112002@gmail.com>
2024-03-26 10:06:56 +0530

    modified connection_file and test file add js and css floting

--- include/db_file/connection_file.php
+++ include/db_file/connection_file.php
@@ -4,7 +4,7 @@
 	$password = "";
 	$dbname = "online_shoping";
 	
-	$conn = mysqli_connect($servername, $username, $password,$dbname);
+	$conn = mysqli_connect($servername,$username,$password,$dbname);
 
 	// Check connection
 	if (!$conn) {
